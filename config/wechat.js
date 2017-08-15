94d938053e659a0651d5c47d8ee1f24529495137
Polar Bottle <shenxinxu@zz11.local>
2017-08-15 15:29:54 -0400

    update

---
+++ config/wechat.js
@@ -0,0 +1,8 @@
+module.exports = {
+  wechat: {
+    appID: 'wxd35250a203467b0f',
+    appSecret: '860a753a9ddf83d7ee8f9ad0b927ddab',
+    token: 'mytoken'
+  }
+}
+
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var sha1 = require('sha1')
+
+var wechatConfig = require('../config/wechat.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,8 +10,23 @@ router.get('/', function(req, res, next) {
   console.log(req.query)
   // skip the validation if the request is from wechat
   res.send(req.query.echostr)
-  // res.json('good')
-  //res.render('index', { title: 'Express' });
+
+  var token = config.wechat.token
+
+  // get value from query string
+  var signature = req.query.signature
+  var nonce = req.query.nonce
+  var timestamp = req.query.timestamp
+  var echostr = req.query.echostr
+
+  var str = [token, timestamp, nonce].sort().join('')
+  var shaResult = sha1(str)
+
+  if (shaResult === signature) {
+    res.send(signature) 
+  } else {
+    res.send ('wechat server validation failed')
+  }
 });
 
 module.exports = router;
